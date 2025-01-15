878e550becf0d3ebfd52d9cf42e104427529aa79
legoChoi <chlx159@gmail.com>
2025-01-15 17:48:30 +0900

    feat: QueryDSL: todo search

--- src/main/java/org/example/expert/domain/todo/repository/TodoQRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoQRepository.java
@@ -1,9 +1,14 @@
 package org.example.expert.domain.todo.repository;
 
+import org.example.expert.domain.todo.dto.response.TodoSearchResponse;
 import org.example.expert.domain.todo.entity.Todo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoQRepository {
     Optional<Todo> findByIdWithUser(Long todoId);
+    Page<TodoSearchResponse> search(String title, String nickname, LocalDateTime startDate, LocalDateTime endDate, Pageable pageable);
 }
