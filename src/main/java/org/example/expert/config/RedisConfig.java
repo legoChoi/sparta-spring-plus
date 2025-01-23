055fdfe4444e79f5f1019ad149fe9ce1cfbd340c
legoChoi <chlx159@gmail.com>
2025-01-23 23:08:39 +0900

    feat: refresh token repository 추가
    - redis

--- src/main/java/org/example/expert/config/RedisConfig.java
+++ src/main/java/org/example/expert/config/RedisConfig.java
@@ -1,6 +1,5 @@
 package org.example.expert.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
